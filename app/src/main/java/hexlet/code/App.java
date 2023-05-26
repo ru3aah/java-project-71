8ea663f17cbf08f2744d5e22d199a48e8a9aac94
Alex Telenkov <ru3aah@gmail.com>
2023-05-26 22:08:57 +0200

    add picocli help in App

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,33 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+
+import java.util.concurrent.Callable;
+
+@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "1.0",
+        description = "Compares two configuration files and shows a difference.")
+
+
+class App implements Callable<Integer> {
+
+
     public static void main(String[] args) {
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
         System.out.println("Hello World!");
+
+    }
+
+    /**
+     * Computes a result, or throws an exception if unable to do so.
+     *
+     * @return computed result
+     * @throws Exception if unable to compute a result
+     */
+    @Override
+    public Integer call() throws Exception {
+        System.out.println("smth went wrong");
+        return null;
     }
 }
