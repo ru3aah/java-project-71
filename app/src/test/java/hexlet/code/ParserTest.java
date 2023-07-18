8ca251321da6750466a294df4332117d2a42c5bb
Alex Telenkov <ru3aah@gmail.com>
2023-07-18 19:13:03 +0300

    minor corections in implementation section at ParserTest to fit requirements

--- app/src/test/java/hexlet/code/ParserTest.java
+++ app/src/test/java/hexlet/code/ParserTest.java
@@ -1,6 +1,11 @@
 package hexlet.code;
 
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
 import java.io.IOException;
 import java.util.Map;
